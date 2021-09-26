61cd0584b0bd84816762d4421eec3e8d74f82d6d
yasser-sulaiman <yassersulaiman.is@gmail.com>
2021-09-26 13:21:17 +0300

    adding files to repository

---
+++ handTracker.py
@@ -0,0 +1,55 @@
+import mediapipe as mp
+import numpy as np
+import cv2
+
+
+class HandTracker():
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+
+        if self.results.multi_hand_landmarks:
+            for handLm in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLm, self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def getPostion(self, img, handNo = 0, draw=True):
+        lmList =[]
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for lm in myHand.landmark:
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                lmList.append((cx, cy))
+
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255,0,255), cv2.FILLED)
+        return lmList
+    
+    def getUpFingers(self, img):
+        pos = self.getPostion(img)
+        self.upfingers = []
+        if pos:
+            #thumb
+            self.upfingers.append((pos[4][1] < pos[3][1] and (pos[5][0]-pos[4][0]> 10)))
+            #index
+            self.upfingers.append((pos[8][1] < pos[7][1] and pos[7][1] < pos[6][1]))
+            #middle
+            self.upfingers.append((pos[12][1] < pos[11][1] and pos[11][1] < pos[10][1]))
+            #ring
+            self.upfingers.append((pos[16][1] < pos[15][1] and pos[15][1] < pos[14][1]))
+            #pinky
+            self.upfingers.append((pos[20][1] < pos[19][1] and pos[19][1] < pos[18][1]))
+        return self.upfingers
+
---
+++ main.py
@@ -0,0 +1,81 @@
+from handTracker import *
+import cv2
+import mediapipe as mp
+
+
+#initilize the habe detector
+detector = HandTracker(detectionCon=0.8)
+
+#initilize the camera 
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+# creating canvas to draw on it
+canvas = np.zeros((720,1280,3), np.uint8)
+
+# define a previous point to be used with drawing a line
+px,py = 0,0
+color = (255,0,0)
+brushSize = 5
+eraserSize = 20
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frame = cv2.flip(frame, 1)
+    detector.findHands(frame)
+    positions = detector.getPostion(frame)
+    upFingers = detector.getUpFingers(frame)
+    if upFingers:
+        if upFingers[1] and upFingers[2]:
+            px, py = 0, 0
+            #blue
+            if 400<positions[8][0]<500 and 0<positions[8][1]<100:
+                color = (255,0,0)
+            #green
+            elif 500<positions[8][0]<600 and 0<positions[8][1]<100:
+                color = (0,255,0)
+            #red
+            if 600<positions[8][0]<700 and 0<positions[8][1]<100:
+                color = (0,0,255)
+            #yellow
+            if 700<positions[8][0]<800 and 0<positions[8][1]<100:
+                color = (0,255,255)
+            #yellow
+            if 800<positions[8][0]<900 and 0<positions[8][1]<100:
+                color = (0,0,0)
+            #print("both index and middle fingers are up")
+        elif upFingers[1] and not upFingers[2]:
+            #print('index finger is up')
+            cv2.circle(frame, positions[8], 10, color,-1)
+            #drawing on the canvas
+            if px == 0 and py == 0:
+                px, py = positions[8]
+            if color == (0,0,0):
+                cv2.line(canvas, (px,py), positions[8], color,eraserSize)
+            else:
+                cv2.line(canvas, (px,py), positions[8], color,brushSize)
+            px, py = positions[8]
+    #pen colors
+    cv2.rectangle(frame, (400,0), (500, 100), (255,0,0), -1)
+    cv2.rectangle(frame, (500,0), (600, 100), (0,255,0), -1)
+    cv2.rectangle(frame, (600,0), (700, 100), (0,0,255), -1)
+    cv2.rectangle(frame, (700,0), (800, 100), (0,255,255), -1)
+    cv2.rectangle(frame, (800,0), (900, 100), (0,0,0), -1)
+    cv2.putText(frame, "Ereaser", (820,50), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,255), 2)
+    
+    #moving the draw to the main image
+    canvasGray = cv2.cvtColor(canvas, cv2.COLOR_BGR2GRAY)
+    _, imgInv = cv2.threshold(canvasGray, 20, 255, cv2.THRESH_BINARY_INV)
+    imgInv = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)
+    frame = cv2.bitwise_and(frame, imgInv)
+    frame = cv2.bitwise_or(frame, canvas)
+
+    cv2.imshow('video', frame)
+    #cv2.imshow('canvas', canvas)
+    k= cv2.waitKey(1)
+    if k == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
